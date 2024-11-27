dd1afed655fd12c5da7cb85fde9b0e695190d07e
joshbaz <jkimbareeba@yahoo.com>
2024-11-27 15:37:30 +0300

    updated version

--- src/6-Views/1Payments/PaymentValidation.jsx
+++ src/6-Views/1Payments/PaymentValidation.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const PaymentValidation = () => {
   return (
-    <div>PaymentValidation</div>
+    <div className="bg-[#D7D5D5] min-h-[100vh] w-full flex flex-col items-center justify-center gap-[20px] relative">
+      <h1>Pa</h1>
+    </div>
   )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,8 @@ import ScrollToTop from "./ScrollToTop.jsx"; // Import ScrollToTop
 import MobilePayValidation from "./6-Views/10PaymentValidations/MobilePayValidation.jsx";
 import PaymentResponse from "./6-Views/10PaymentValidations/PaymentResponse.jsx";
 import NyatiFlixSoon from "./6-Views/14ComingSoon/NyatiFlixSoon.jsx";
-import DeletePolicy from "./6-Views/15Policies/DeletePolicy.jsx";
-import PrivacyPolicy from "./6-Views/15Policies/PrivacyPolicy.jsx";
-import TermsOfService from "./6-Views/15Policies/TermsOfService.jsx";
+
+
 import ErrorPage from "./6-Views/0ErrorPage/ErrorPage.jsx";
 import PaymentValidation from "./6-Views/1Payments/PaymentValidation.jsx";
 
@@ -27,9 +26,7 @@ function App() {
             <Route path="/pay-response" element={<PaymentResponse />} />
             <Route path="/mpay-validate" element={<MobilePayValidation />} />
             <Route path="/comingsoon" element={<NyatiFlixSoon />} />
-            <Route path="/policies/deletepolicy" element={<DeletePolicy />} />
-            <Route path="/policies/privacypolicy" element={<PrivacyPolicy />} />
-            <Route path="/policies/termsofservice" element={<TermsOfService />} />
+  
             <Route path="*" element={<ErrorPage />} />
           </Routes>
         </ScrollToTop>
