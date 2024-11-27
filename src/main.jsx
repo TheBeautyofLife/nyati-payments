f8e5986a06c80e93c465422eb07a3707dad6ba58
joshbaz <jkimbareeba@yahoo.com>
2024-11-27 17:34:57 +0300

    updates:redirects

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from './5-Store/reduxState/store.js'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Provider store={store}><App /> </Provider>);
